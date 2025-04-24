65927c5562256d6bed148c11575d48c26926f3cc
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-04-24 10:21:01 +0800

    fix: eslint import order

--- packages/react/runtime/jsx-dev-runtime/index.d.ts
+++ packages/react/runtime/jsx-dev-runtime/index.d.ts
@@ -1,10 +1,12 @@
 // Copyright 2024 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
+import { JSX as _JSX } from 'react';
+
+import { IntrinsicElements as _IntrinsicElements } from '@lynx-js/types';
+
 export { jsxDEV, Fragment } from 'react/jsx-dev-runtime';
 export { jsx, jsxs } from 'react/jsx-runtime';
-import { IntrinsicElements as _IntrinsicElements } from '@lynx-js/types';
-import { JSX as _JSX } from 'react';
 
 export namespace JSX {
   interface IntrinsicElements extends _IntrinsicElements {}
--- packages/react/runtime/jsx-runtime/index.d.ts
+++ packages/react/runtime/jsx-runtime/index.d.ts
@@ -1,9 +1,11 @@
 // Copyright 2024 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
-export { jsx, jsxs, Fragment } from 'react/jsx-runtime';
-import { IntrinsicElements as _IntrinsicElements } from '@lynx-js/types';
 import { JSX as _JSX } from 'react';
+
+import { IntrinsicElements as _IntrinsicElements } from '@lynx-js/types';
+
+export { jsx, jsxs, Fragment } from 'react/jsx-runtime';
 
 export namespace JSX {
   interface IntrinsicElements extends _IntrinsicElements {}
