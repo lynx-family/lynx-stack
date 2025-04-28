0ba51a14d8d969cec1f568b9e5fc5276d53df532
BitterGourd <91231822+gaoachao@users.noreply.github.com>
2025-04-28 15:05:06 +0800

    chore: add chunkSplit config

--- examples/react/lynx.config.js
+++ examples/react/lynx.config.js
@@ -5,6 +5,15 @@ import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
 const enableBundleAnalysis = !!process.env['RSPEEDY_BUNDLE_ANALYSIS'];
 
 export default defineConfig({
+  // performance: {
+  //   chunkSplit: {
+  //     strategy: 'split-by-experience',
+  //     override: {
+  //       // See: https://github.com/web-infra-dev/rspack/issues/9812
+  //       filename: '[name].[contenthash:8].js',
+  //     },
+  //   },
+  // },
   plugins: [
     pluginReactLynx(),
     pluginQRCode({
