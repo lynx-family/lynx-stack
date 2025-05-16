0ddd91862392700a21c6c28c65ad1b11f5aa103b
Yradex <11014207+Yradex@users.noreply.github.com>
2025-05-16 18:15:15 +0800

    Update ctxTrace.ts

--- packages/react/worklet-runtime/src/ctxTrace.ts
+++ packages/react/worklet-runtime/src/ctxTrace.ts
@@ -2,8 +2,6 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 
-import * as console from 'node:console';
-
 import type { ClosureValueType, Worklet } from './bindings/types.js';
 
 interface CtxTrace {
