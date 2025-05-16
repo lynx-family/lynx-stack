7888f2147cd39399fe4024e2a15aeae00cdefd53
Yradex <11014207+Yradex@users.noreply.github.com>
2025-05-16 17:54:35 +0800

    feat(react/runtime): add context tracking for worklet execution

---
+++ packages/react/worklet-runtime/src/ctxTrace.ts
@@ -0,0 +1,24 @@
+// Copyright 2024 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+
+import * as console from 'node:console';
+
+import type { ClosureValueType, Worklet } from './bindings/types.js';
+
+interface CtxTrace {
+  ctx: Worklet;
+}
+
+export let currentCtx: CtxTrace | undefined;
+
+export function traceCtxCall(ctx: Worklet, _params: ClosureValueType[]): void {
+  console.log('traceCtxCall', ctx);
+  currentCtx = {
+    ctx,
+  };
+}
+
+export function clearCurrentCtx(): void {
+  currentCtx = undefined;
+}
