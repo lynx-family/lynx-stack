215409f4de9ed802f31d1c7dac88eee2a8db1a79
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-05-20 15:55:11 +0800

    fix: do not make `defineDCE` internal

--- packages/react/transform/index.d.ts
+++ packages/react/transform/index.d.ts
@@ -339,9 +339,59 @@ export interface CssScopeVisitorConfig {
   /** @public */
   filename: string
 }
-/** @internal */
+/**
+ * {@inheritdoc PluginReactLynxOptions.defineDCE}
+ * @public
+ */
 export interface DefineDceVisitorConfig {
-  /** @internal */
+  /**
+   * @public
+   * Replaces variables in your code with other values or expressions at compile time.
+   *
+   * @remarks
+   * Default defines: `__MAIN_THREAD__`, `__BACKGROUND__`, `__REACTLYNX2__`, `__REACTLYNX3__`
+   *
+   * Caveat: differences between `source.define`
+   *
+   * `defineDCE` happens before transforming `background-only` directives.
+   * So it's useful for eliminating code that is only used in the background from main-thread.
+   *
+   * @example
+   *
+   * ```js
+   * import { defineConfig } from '@lynx-js/rspeedy'
+   * import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+   *
+   * export default defineConfig({
+   *   plugins: [
+   *     pluginReactLynx({
+   *       defineDCE: {
+   *         define: {
+   *           __FOO__: 'false',
+   *           'process.env.PLATFORM': '"lynx"',
+   *         },
+   *       },
+   *     })
+   *   ],
+   * })
+   * ```
+   *
+   * Then, `__FOO__` and `process.env.PLATFORM` could be used in source code.
+   *
+   * ```
+   * if (process.env.PLATFORM === 'lynx') {
+   *   console.log('lynx')
+   * }
+   *
+   * function FooOrBar() {
+   *   if (__FOO__) {
+   *     return <text>foo</text>
+   *   } else {
+   *     return <text>bar</text>
+   *   }
+   * }
+   * ```
+   */
   define: Record<string, string>
 }
 export interface DirectiveDceVisitorConfig {
--- packages/react/transform/src/swc_plugin_define_dce/mod.rs
+++ packages/react/transform/src/swc_plugin_define_dce/mod.rs
@@ -2,11 +2,57 @@ use std::{collections::HashMap, fmt::Debug};
 
 use napi_derive::napi;
 
-/// @internal
+/// {@inheritdoc PluginReactLynxOptions.defineDCE}
+/// @public
 #[napi(object)]
 #[derive(Clone, Debug)]
 pub struct DefineDCEVisitorConfig {
-  /// @internal
+  /// @public
+  /// Replaces variables in your code with other values or expressions at compile time.
+  ///
+  /// @remarks
+  /// Default defines: `__MAIN_THREAD__`, `__BACKGROUND__`, `__REACTLYNX2__`, `__REACTLYNX3__`
+  ///
+  /// Caveat: differences between `source.define`
+  ///
+  /// `defineDCE` happens before transforming `background-only` directives.
+  /// So it's useful for eliminating code that is only used in the background from main-thread.
+  ///
+  /// @example
+  ///
+  /// ```js
+  /// import { defineConfig } from '@lynx-js/rspeedy'
+  /// import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
+  ///
+  /// export default defineConfig({
+  ///   plugins: [
+  ///     pluginReactLynx({
+  ///       defineDCE: {
+  ///         define: {
+  ///           __FOO__: 'false',
+  ///           'process.env.PLATFORM': '"lynx"',
+  ///         },
+  ///       },
+  ///     })
+  ///   ],
+  /// })
+  /// ```
+  ///
+  /// Then, `__FOO__` and `process.env.PLATFORM` could be used in source code.
+  ///
+  /// ```
+  /// if (process.env.PLATFORM === 'lynx') {
+  ///   console.log('lynx')
+  /// }
+  ///
+  /// function FooOrBar() {
+  ///   if (__FOO__) {
+  ///     return <text>foo</text>
+  ///   } else {
+  ///     return <text>bar</text>
+  ///   }
+  /// }
+  /// ```
   pub define: HashMap<String, String>,
 }
 
