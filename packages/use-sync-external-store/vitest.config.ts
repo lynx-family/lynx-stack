eb3fc606eb11d9eae2e7e3243ea2bd46352aa84c
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-05-27 15:33:50 +0800

    test: migrate tests from `react`

--- packages/use-sync-external-store/vitest.config.ts
+++ packages/use-sync-external-store/vitest.config.ts
@@ -1,5 +1,9 @@
-import { defineProject } from 'vitest/config';
+import { defineProject, mergeConfig } from 'vitest/config';
 import type { UserWorkspaceConfig } from 'vitest/config';
+
+import { createVitestConfig } from '@lynx-js/react/testing-library/vitest-config';
+
+const defaultConfig = await createVitestConfig();
 
 const config: UserWorkspaceConfig = defineProject({
   test: {
@@ -7,4 +11,6 @@ const config: UserWorkspaceConfig = defineProject({
   },
 });
 
-export default config;
+const mergedConfig: UserWorkspaceConfig = mergeConfig(defaultConfig, config);
+
+export default mergedConfig;
