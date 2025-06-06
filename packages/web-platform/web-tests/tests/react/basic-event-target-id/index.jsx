lynx-family/lynx-stack

commit c014327ad0cf599b32d4182d95116b46c35f5fa5

refactor: implement mts apis in closure pattern (#1004)
Before this commit, the globalThis of mts may leaks some internal
values, such as _rootDom.

After this commit, we scope them in a closure rather than assign it on
the globalThis object.

packages/web-platform/web-mainthread-apis/src/utils/createExposureService.ts
@@ -4,12 +4,15 @@
 import {
   lynxUniqueIdAttribute,
   type ExposureWorkerEvent,
+  type postExposureEndpoint,
+  type RpcCallType,
 } from '@lynx-js/web-constants';
 import { createCrossThreadEvent } from './createCrossThreadEvent.js';
-import type { MainThreadRuntime } from '../MainThreadRuntime.js';
 
-export function createExposureService(runtime: MainThreadRuntime) {
-  const postExposure = runtime.config.callbacks.postExposure;
+export function createExposureService(
+  rootDom: Pick<EventTarget, 'addEventListener'>,
+  postExposure: RpcCallType<typeof postExposureEndpoint>,
+) {
   let working = true;
   let exposureCache: ExposureWorkerEvent[] = [];
   let disexposureCache: ExposureWorkerEvent[] = [];
@@ -43,10 +46,10 @@ export function createExposureService(runtime: MainThreadRuntime) {
       });
     }
   }, 1000 / 20);
-  runtime._rootDom.addEventListener('exposure', exposureEventHandler, {
+  rootDom.addEventListener('exposure', exposureEventHandler, {
     passive: true,
   });
-  runtime._rootDom.addEventListener('disexposure', exposureEventHandler, {
+  rootDom.addEventListener('disexposure', exposureEventHandler, {
     passive: true,
   });
 

packages/web-platform/offscreen-document/src/webworker/OffscreenElement.ts
@@ -55,17 +55,9 @@ export class OffscreenElement extends EventTarget {
     return this[_style];
   }
 
-  get id(): string {
-    return this[_attributes].get('id') ?? '';
-  }
-
   set id(value: string) {
     this[_attributes].set('id', value);
     this.setAttribute('id', value);
-  }
-
-  get className(): string {
-    return this[_attributes].get('class') ?? '';
   }
 
   get children(): OffscreenElement[] {

packages/web-platform/web-constants/src/types/Element.ts
@@ -1,6 +1,38 @@
 // Copyright 2023 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
+
+import type { LynxEventType } from './EventType.js';
+
+export interface LynxRuntimeInfo {
+  eventHandlerMap: Record<string, {
+    capture: {
+      type: LynxEventType;
+      handler: string | { type: 'worklet'; value: unknown };
+    } | undefined;
+    bind: {
+      type: LynxEventType;
+      handler: string | { type: 'worklet'; value: unknown };
+    } | undefined;
+  }>;
+  componentAtIndex?: ComponentAtIndexCallback;
+  enqueueComponent?: EnqueueComponentCallback;
+}
+
+export type ComponentAtIndexCallback = (
+  list: WebFiberElementImpl,
+  listID: number,
+  cellIndex: number,
+  operationID: number,
+  enableReuseNotification: boolean,
+) => void;
+
+export type EnqueueComponentCallback = (
+  list: WebFiberElementImpl,
+  listID: number,
+  sign: number,
+) => void;
+
 export const enum AnimationOperation {
   START = 0,
   PLAY,
@@ -14,4 +46,50 @@ export interface ElementAnimationOptions {
   id: string;
   keyframes?: any;
   timingOptions?: Record<string, any>;
+}
+
+export interface WebFiberElementImpl {
+  getAttributeNames: () => string[];
+  getAttribute: (name: string) => string | null;
+  setAttribute: (name: string, value: string) => void;
+  removeAttribute: (name: string) => void;
+  append: (...children: WebFiberElementImpl[]) => void;
+  appendChild: (child: WebFiberElementImpl) => void;
+  removeChild: (child: WebFiberElementImpl) => WebFiberElementImpl;
+  insertBefore: (
+    child: WebFiberElementImpl,
+    ref?: WebFiberElementImpl | null,
+  ) => WebFiberElementImpl;
+  replaceWith: (...newElement: WebFiberElementImpl[]) => void;
+  addEventListener: (
+    type: string,
+    handler: (ev: Event) => void,
+    options: {
+      capture?: boolean;
+      passive?: boolean;
+    },
+  ) => void;
+  removeEventListener: (
+    type: string,
+    handler: (ev: Event) => void,
+    options?: {
+      capture?: boolean;
+    },
+  ) => void;
+  innerHTML: string;
+  readonly tagName: string;
+  readonly firstElementChild: WebFiberElementImpl | null;
+  readonly children: WebFiberElementImpl[];
+  readonly parentElement: WebFiberElementImpl | null;
+  readonly parentNode: WebFiberElementImpl | null;
+  readonly lastElementChild: WebFiberElementImpl | null;
+  readonly nextElementSibling: WebFiberElementImpl | null;
+  readonly style: {
+    removeProperty(name: string): void;
+    setProperty(
+      name: string,
+      value: string,
+      priority?: 'important' | '' | null,
+    ): void;
+  };
 }

packages/web-platform/web-constants/src/types/MainThreadGlobalThis.ts
@@ -1,0 +1,344 @@
+import type { systemInfo } from '../constants.js';
+import type { Cloneable } from './Cloneable.js';
+import type {
+  ComponentAtIndexCallback,
+  EnqueueComponentCallback,
+  WebFiberElementImpl,
+} from './Element.js';
+import type { LynxEventType } from './EventType.js';
+import type { FlushElementTreeOptions } from './FlushElementTreeOptions.js';
+import type { MainThreadLynx } from './MainThreadLynx.js';
+import type { ProcessDataCallback } from './ProcessDataCallback.js';
+
+type ElementPAPIEventHandler =
+  | string
+  | { type: 'worklet'; value: unknown }
+  | undefined;
+
+export type AddEventPAPI = (
+  element: WebFiberElementImpl,
+  eventType: LynxEventType,
+  eventName: string,
+  newEventHandler: ElementPAPIEventHandler,
+) => void;
+
+export type GetEventPAPI = (
+  element: WebFiberElementImpl,
+  eventName: string,
+  eventType: LynxEventType,
+) => ElementPAPIEventHandler;
+
+export type GetEventsPAPI = (
+  element: WebFiberElementImpl,
+) => {
+  type: LynxEventType;
+  name: string;
+  function: ElementPAPIEventHandler;
+}[];
+
+export type SetEventsPAPI = (
+  element: WebFiberElementImpl,
+  listeners: {
+    type: LynxEventType;
+    name: string;
+    function: ElementPAPIEventHandler;
+  }[],
+) => void;
+
+export type AppendElementPAPI = (
+  parent: WebFiberElementImpl,
+  child: WebFiberElementImpl,
+) => void;
+
+export type ElementIsEqualPAPI = (
+  left: WebFiberElementImpl,
+  right: WebFiberElementImpl,
+) => boolean;
+
+export type FirstElementPAPI = (
+  element: WebFiberElementImpl,
+) => WebFiberElementImpl | null;
+
+export type GetChildrenPAPI = (
+  element: WebFiberElementImpl,
+) => WebFiberElementImpl[];
+
+export type GetParentPAPI = (
+  element: WebFiberElementImpl,
+) => WebFiberElementImpl | null;
+
+export type InsertElementBeforePAPI = (
+  parent: WebFiberElementImpl,
+  child: WebFiberElementImpl,
+  ref?: WebFiberElementImpl | null,
+) => WebFiberElementImpl;
+
+export type LastElementPAPI = (
+  element: WebFiberElementImpl,
+) => WebFiberElementImpl | null;
+
+export type NextElementPAPI = (
+  element: WebFiberElementImpl,
+) => WebFiberElementImpl | null;
+
+export type RemoveElementPAPI = (
+  parent: WebFiberElementImpl,
+  child: WebFiberElementImpl,
+) => WebFiberElementImpl;
+
+export type ReplaceElementPAPI = (
+  newElement: WebFiberElementImpl,
+  oldElement: WebFiberElementImpl,
+) => void;
+
+export type ReplaceElementsPAPI = (
+  parent: WebFiberElementImpl,
+  newChildren: WebFiberElementImpl[] | WebFiberElementImpl,
+  oldChildren?: WebFiberElementImpl[] | WebFiberElementImpl | null | undefined,
+) => void;
+
+export type AddConfigPAPI = (
+  element: WebFiberElementImpl,
+  type: string,
+  value: Cloneable,
+) => void;
+
+export type AddDatasetPAPI = (
+  element: WebFiberElementImpl,
+  key: string,
+  value: Cloneable,
+) => void;
+
+export type GetDatasetPAPI = (
+  element: WebFiberElementImpl,
+) => Record<string, Cloneable>;
+
+export type GetDataByKeyPAPI = (
+  element: WebFiberElementImpl,
+  key: string,
+) => Cloneable | undefined;
+
+export type GetAttributesPAPI = (
+  element: WebFiberElementImpl,
+) => Record<string, string>;
+
+export type GetComponentIdPAPI = (
+  element: WebFiberElementImpl,
+) => string | null;
+
+export type GetElementConfigPAPI = (
+  element: WebFiberElementImpl,
+) => Record<string, Cloneable>;
+
+export type GetElementUniqueIDPAPI = (
+  element: WebFiberElementImpl,
+) => number;
+
+export type GetIDPAPI = (
+  element: WebFiberElementImpl,
+) => string | null;
+
+export type GetTagPAPI = (
+  element: WebFiberElementImpl,
+) => string;
+
+export type SetConfigPAPI = (
+  element: WebFiberElementImpl,
+  config: Record<string, Cloneable>,
+) => void;
+
+export type SetDatasetPAPI = (
+  element: WebFiberElementImpl,
+  dataset: Record<string, Cloneable>,
+) => void;
+
+export type SetIDPAPI = (
+  element: WebFiberElementImpl,
+  id: string | null,
+) => void;
+
+export type UpdateComponentIDPAPI = (
+  element: WebFiberElementImpl,
+  componentID: string,
+) => void;
+
+export type GetClassesPAPI = (
+  element: WebFiberElementImpl,
+) => string[];
+
+export type CreateViewPAPI = (
+  parentComponentUniqueID: number,
+) => WebFiberElementImpl;
+
+export type SwapElementPAPI = (
+  childA: WebFiberElementImpl,
+  childB: WebFiberElementImpl,
+) => void;
+
+export type UpdateListInfoAttributeValue = {
+  insertAction: {
+    position: number;
+  }[];
+  removeAction: {
+    position: number;
+  }[];
+};
+export type SetAttributePAPI = (
+  element: WebFiberElementImpl,
+  key: string,
+  value: string | null | undefined | UpdateListInfoAttributeValue,
+) => void;
+
+export type UpdateListCallbacksPAPI = (
+  element: WebFiberElementImpl,
+  componentAtIndex: ComponentAtIndexCallback,
+  enqueueComponent: EnqueueComponentCallback,
+) => void;
+
+export type CreateTextPAPI = CreateViewPAPI;
+
+export type CreateRawTextPAPI = (text: string) => WebFiberElementImpl;
+
+export type CreateImagePAPI = CreateViewPAPI;
+
+export type CreateScrollViewPAPI = CreateViewPAPI;
+
+export type CreateWrapperElementPAPI = CreateViewPAPI;
+
+export type CreateComponentPAPI = (
+  componentParentUniqueID: number,
+  componentID: string,
+  cssID: number,
+  entryName: string,
+  name: string,
+  path: string,
+  config: Record<string, Cloneable> | null | undefined,
+  info: Record<string, Cloneable> | null | undefined,
+) => WebFiberElementImpl;
+
+export type CreateElementPAPI = (
+  tagName: string,
+  parentComponentUniqueId: number,
+  info?: Record<string, Cloneable> | null | undefined,
+) => WebFiberElementImpl;
+
+export type CreatePagePAPI = (
+  componentID: string,
+  cssID: number,
+  info: Record<string, Cloneable> | null | undefined,
+) => WebFiberElementImpl;
+
+export type CreateListPAPI = (
+  parentComponentUniqueId: number,
+  componentAtIndex: ComponentAtIndexCallback,
+  enqueueComponent: EnqueueComponentCallback,
+) => WebFiberElementImpl;
+
+export type AddClassPAPI = (
+  element: WebFiberElementImpl,
+  className: string,
+) => void;
+
+export type SetClassesPAPI = (
+  element: WebFiberElementImpl,
+  classNames: string | null,
+) => void;
+
+export type AddInlineStylePAPI = (
+  element: WebFiberElementImpl,
+  key: number | string,
+  value: string | number | null | undefined,
+) => void;
+
+export type SetInlineStylesPAPI = (
+  element: WebFiberElementImpl,
+  value: string | Record<string, string> | undefined,
+) => void;
+
+export type SetCSSIdPAPI = (
+  elements: WebFiberElementImpl[],
+  cssId: number | null,
+) => void;
+
+export type GetPageElementPAPI = () => WebFiberElementImpl | undefined;
+
+export type GetTemplatePartsPAPI = (
+  templateElement: WebFiberElementImpl,
+) => Record<string, WebFiberElementImpl> | undefined;
+
+export interface MainThreadGlobalThis {
+  __AddEvent: AddEventPAPI;
+  __GetEvent: GetEventPAPI;
+  __GetEvents: GetEventsPAPI;
+  __SetEvents: SetEventsPAPI;
+  __AppendElement: AppendElementPAPI;
+  __ElementIsEqual: ElementIsEqualPAPI;
+  __FirstElement: FirstElementPAPI;
+  __GetChildren: GetChildrenPAPI;
+  __GetParent: GetParentPAPI;
+  __InsertElementBefore: InsertElementBeforePAPI;
+  __LastElement: LastElementPAPI;
+  __NextElement: NextElementPAPI;
+  __RemoveElement: RemoveElementPAPI;
+  __ReplaceElement: ReplaceElementPAPI;
+  __ReplaceElements: ReplaceElementsPAPI;
+  __AddConfig: AddConfigPAPI;
+  __AddDataset: AddDatasetPAPI;
+  __GetDataset: GetDatasetPAPI;
+  __GetDataByKey: GetDataByKeyPAPI;
+  __GetAttributes: GetAttributesPAPI;
+  __GetComponentID: GetComponentIdPAPI;
+  __GetElementConfig: GetElementConfigPAPI;
+  __GetElementUniqueID: GetElementUniqueIDPAPI;
+  __GetID: GetIDPAPI;
+  __GetTag: GetTagPAPI;
+  __SetConfig: SetConfigPAPI;
+  __GetConfig: GetElementConfigPAPI;
+  __SetDataset: SetDatasetPAPI;
+  __SetID: SetIDPAPI;
+  __UpdateComponentID: UpdateComponentIDPAPI;
+  __GetClasses: GetClassesPAPI;
+  __CreateView: CreateViewPAPI;
+  __SwapElement: SwapElementPAPI;
+  __CreateText: CreateTextPAPI;
+  __CreateRawText: CreateRawTextPAPI;
+  __CreateImage: CreateImagePAPI;
+  __CreateScrollView: CreateScrollViewPAPI;
+  __CreateWrapperElement: CreateWrapperElementPAPI;
+  __CreateComponent: CreateComponentPAPI;
+  __CreateElement: CreateElementPAPI;
+  __CreatePage: CreatePagePAPI;
+  __CreateList: CreateListPAPI;
+  __SetAttribute: SetAttributePAPI;
+  __UpdateListCallbacks: UpdateListCallbacksPAPI;
+  __AddClass: AddClassPAPI;
+  __SetClasses: SetClassesPAPI;
+  __AddInlineStyle: AddInlineStylePAPI;
+  __SetInlineStyles: SetInlineStylesPAPI;
+  __SetCSSId: SetCSSIdPAPI;
+  __GetPageElement: GetPageElementPAPI;
+  __GetTemplateParts: GetTemplatePartsPAPI;
+  __globalProps: unknown;
+  SystemInfo: typeof systemInfo;
+  globalThis?: MainThreadGlobalThis;
+  lynx: MainThreadLynx;
+  processData?: ProcessDataCallback;
+  ssrEncode?: () => string;
+  ssrHydrate?: (encodeData?: string) => void;
+  _ReportError: (error: string, _: unknown) => void;
+  __OnLifecycleEvent: (lifeCycleEvent: Cloneable) => void;
+  __LoadLepusChunk: (path: string) => boolean;
+  __FlushElementTree: (
+    _subTree: unknown,
+    options: FlushElementTreeOptions,
+  ) => void;
+  /**
+   * private fields
+   */
+  _updateVars: () => void;
+  __lynxGlobalBindingValues: Record<string, unknown>;
+  // the following methods is assigned by the main thread user code
+  renderPage: ((data: unknown) => void) | undefined;
+  updatePage?: (data: Cloneable, options?: Record<string, string>) => void;
+  runWorklet?: (obj: unknown, event: unknown) => void;
+}

packages/web-platform/web-constants/src/types/MainThreadLynx.ts
@@ -1,0 +1,10 @@
+import type { Cloneable } from './Cloneable.js';
+import type { LynxContextEventTarget } from './LynxContextEventTarget.js';
+export interface MainThreadLynx {
+  getJSContext: () => LynxContextEventTarget;
+  requestAnimationFrame: (cb: () => void) => number;
+  cancelAnimationFrame: (handler: number) => void;
+  __globalProps: unknown;
+  getCustomSectionSync: (key: string) => Cloneable;
+  markPipelineTiming: (pipelineId: string, timingKey: string) => void;
+}

packages/web-platform/web-constants/src/types/index.ts
@@ -13,3 +13,5 @@ export * from './NapiModules.js';
 export * from './FlushElementTreeOptions.js';
 export * from './LynxContextEventTarget.js';
 export * from './Element.js';
+export * from './MainThreadGlobalThis.js';
+export * from './MainThreadLynx.js';

packages/web-platform/web-core/src/uiThread/createRenderAllOnUI.ts
@@ -2,8 +2,8 @@ import type {
   StartMainThreadContextConfig,
   RpcCallType,
   updateDataEndpoint,
+  MainThreadGlobalThis,
 } from '@lynx-js/web-constants';
-import type { MainThreadRuntime } from '@lynx-js/web-mainthread-apis';
 import { Rpc } from '@lynx-js/web-worker-rpc';
 
 const {
@@ -35,15 +35,15 @@ export function createRenderAllOnUI(
       callbacks.onError?.();
     },
   );
-  let runtime!: MainThreadRuntime;
+  let mtsGlobalThis!: MainThreadGlobalThis;
   const start = async (configs: StartMainThreadContextConfig) => {
     const mainThreadRuntime = startMainThread(configs);
-    runtime = await mainThreadRuntime;
+    mtsGlobalThis = await mainThreadRuntime;
   };
   const updateDataMainThread: RpcCallType<typeof updateDataEndpoint> = async (
     ...args
   ) => {
-    runtime.updatePage?.(...args);
+    mtsGlobalThis.updatePage?.(...args);
   };
   return {
     start,

commit 26c34cb400c563daeff1b666c38e3e1f0f3d0c74

fix: target.id is undefined
this issue is caused by #1004

packages/web-platform/web-constants/src/types/EventType.ts
@@ -3,6 +3,7 @@
 // LICENSE file in the root directory of this source tree.
 
 import type { Cloneable } from './Cloneable.js';
+import type { WebFiberElementImpl } from './Element.js';
 
 export type LynxEventType =
   | 'bindEvent'
@@ -14,7 +15,7 @@ export interface LynxCrossThreadEventTarget {
   dataset: {
     [key: string]: Cloneable;
   };
-  id?: string;
+  id: string | null;
   uniqueId: number;
 }
 
@@ -56,3 +57,17 @@ export type ExposureEvent = {
 export type ExposureWorkerEvent =
   & LynxCrossThreadEvent<ExposureEventDetail>
   & ExposureEventDetail;
+
+export type MinimalRawEventObject = {
+  type: string;
+  target: Pick<WebFiberElementImpl, 'getAttribute'> | null;
+  currentTarget: Pick<WebFiberElementImpl, 'getAttribute'> | null;
+  detail?: Cloneable;
+  isTrusted: boolean;
+  timeStamp: number;
+  propertyName?: string; // For transition events
+  animationName?: string; // For animation events
+  touches?: unknown[]; // For touch events
+  targetTouches?: unknown[]; // For touch events
+  changedTouches?: unknown[]; // For touch events
+};

packages/web-platform/web-mainthread-apis/src/createMainThreadGlobalThis.ts
@@ -55,6 +55,7 @@ import {
   type SetClassesPAPI,
   type GetTemplatePartsPAPI,
   type GetPageElementPAPI,
+  type MinimalRawEventObject,
 } from '@lynx-js/web-constants';
 import { globalMuteableVars } from '@lynx-js/web-constants';
 import { createMainThreadLynx } from './createMainThreadLynx.js';
@@ -211,7 +212,7 @@ export function createMainThreadGlobalThis(
         : runtimeInfo.eventHandlerMap[lynxEventName]?.bind
           ?.handler;
       const crossThreadEvent = createCrossThreadEvent(
-        event,
+        event as MinimalRawEventObject,
         lynxEventName,
       );
       if (typeof hname === 'string') {

packages/web-platform/web-mainthread-apis/src/utils/createCrossThreadEvent.ts
@@ -7,6 +7,7 @@ import {
   type Cloneable,
   type CloneableObject,
   type LynxCrossThreadEvent,
+  type MinimalRawEventObject,
 } from '@lynx-js/web-constants';
 
 function toCloneableObject(obj: any): CloneableObject {
@@ -24,7 +25,7 @@ function toCloneableObject(obj: any): CloneableObject {
 }
 
 export function createCrossThreadEvent(
-  domEvent: Event,
+  domEvent: MinimalRawEventObject,
   eventName: string,
 ): LynxCrossThreadEvent {
   const targetElement = domEvent.target as HTMLElement;
@@ -39,17 +40,17 @@ export function createCrossThreadEvent(
   if (type.match(/^transition/)) {
     Object.assign(params, {
       'animation_type': 'keyframe-animation',
-      'animation_name': (domEvent as TransitionEvent).propertyName,
+      'animation_name': domEvent.propertyName,
       new_animator: true, // we support the new_animator only
     });
   } else if (type.match(/animation/)) {
     Object.assign(params, {
       'animation_type': 'keyframe-animation',
-      'animation_name': (domEvent as AnimationEvent).animationName,
+      'animation_name': domEvent.animationName,
       new_animator: true, // we support the new_animator only
     });
   } else if (type.startsWith('touch')) {
-    const touchEvent = domEvent as TouchEvent;
+    const touchEvent = domEvent;
     const touch = [...touchEvent.touches as unknown as Touch[]];
     const targetTouches = [...touchEvent.targetTouches as unknown as Touch[]];
     const changedTouches = [...touchEvent.changedTouches as unknown as Touch[]];
@@ -82,13 +83,13 @@ export function createCrossThreadEvent(
     type: eventName,
     timestamp: domEvent.timeStamp,
     target: {
-      id: targetElement.id,
+      id: targetElement.getAttribute('id'),
       dataset: targetDataset,
       uniqueId: Number(targetElement.getAttribute(lynxUniqueIdAttribute)),
     },
     currentTarget: currentTargetElement
       ? {
-        id: currentTargetElement.id,
+        id: currentTargetElement.getAttribute('id'),
         dataset: currentTargetDataset,
         uniqueId: Number(
           currentTargetElement.getAttribute(lynxUniqueIdAttribute),

packages/web-platform/web-mainthread-apis/src/utils/createExposureService.ts
@@ -4,6 +4,7 @@
 import {
   lynxUniqueIdAttribute,
   type ExposureWorkerEvent,
+  type MinimalRawEventObject,
   type postExposureEndpoint,
   type RpcCallType,
 } from '@lynx-js/web-constants';
@@ -19,7 +20,7 @@ export function createExposureService(
   const onScreen = new Map<string, ExposureWorkerEvent>();
   function exposureEventHandler(ev: Event) {
     const exposureEvent = createCrossThreadEvent(
-      ev,
+      ev as MinimalRawEventObject,
       ev.type,
     ) as ExposureWorkerEvent;
     exposureEvent.detail['unique-id'] = parseFloat(

packages/web-platform/web-tests/tests/react.spec.ts
@@ -96,6 +96,15 @@ test.describe('reactlynx3 tests', () => {
       await target.click();
       await expect(await target.getAttribute('style')).toContain('pink');
     });
+    test('basic-event-target-id', async ({ page }, { title }) => {
+      await goto(page, title);
+      await wait(100);
+      const target = page.locator('#target');
+      await target.click();
+      await expect(await target.getAttribute('style')).toContain('green');
+      await target.click();
+      await expect(await target.getAttribute('style')).toContain('pink');
+    });
     test('basic-class-selector', async ({ page }, { title }) => {
       await goto(page, title);
       await wait(100);

packages/web-platform/web-tests/tests/react/basic-event-target-id/index.jsx
@@ -1,0 +1,21 @@
+// Copyright 2023 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+import { useState, root } from '@lynx-js/react';
+function App() {
+  const [color, setColor] = useState('pink');
+  return (
+    <view
+      id='target'
+      bindTap={(ev) =>
+        ev.target.id === 'target' && ev.currentTarget.id === 'target'
+        && setColor(color === 'pink' ? 'green' : 'pink')}
+      style={{
+        height: '100px',
+        width: '100px',
+        background: color,
+      }}
+    />
+  );
+}
+root.render(<App />);