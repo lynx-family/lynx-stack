4f38acc1e3f40c0d393375b04a0a1f45e2ac0578
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-06-16 12:30:58 +0800

    ci: make the all-on-ui to be the default rendering mode
    [Feature]: Breaking Change: set the all-on-ui mode to be the default render #1080

--- packages/web-platform/web-tests/tests/react.spec.ts
+++ packages/web-platform/web-tests/tests/react.spec.ts
@@ -4,8 +4,8 @@
 import { swipe, dragAndHold } from './utils.js';
 import { test, expect } from './coverage-fixture.js';
 import type { Page } from '@playwright/test';
-const ALL_ON_UI = !!process.env['ALL_ON_UI'];
-
+const ENABLE_MULTI_THREAD = !!process.env['ENABLE_MULTI_THREAD'];
+const ALL_ON_UI = !ENABLE_MULTI_THREAD;
 const wait = async (ms: number) => {
   await new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -673,14 +673,14 @@ test.describe('reactlynx3 tests', () => {
       await goto(page, title);
       const target = page.locator('#target');
       await expect(target).toHaveCSS('background-color', 'rgb(255, 192, 203)'); // pink
-      expect(page.workers().length).toStrictEqual(ALL_ON_UI ? 2 : 3);
+      expect(page.workers().length).toStrictEqual(ENABLE_MULTI_THREAD ? 3 : 2);
     });
 
     test('api-preheat-at-least-one', async ({ page }, { title }) => {
       await goto(page, title);
       const target = page.locator('#target');
       await expect(target).toHaveCSS('background-color', 'rgb(255, 192, 203)'); // pink
-      expect(page.workers().length).toBe(ALL_ON_UI ? 2 : 3);
+      expect(page.workers().length).toBe(ENABLE_MULTI_THREAD ? 3 : 2);
       await page.evaluate(() => {
         document.body.querySelector('lynx-view')?.remove();
       });
