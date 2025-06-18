f7c7e6e76bf8c3e95f5ae4b5d0adb67615f22ce1
Yradex <11014207+Yradex@users.noreply.github.com>
2025-06-18 20:59:04 +0800

    autofix

--- packages/react/runtime/src/lynx/runWithForce.ts
+++ packages/react/runtime/src/lynx/runWithForce.ts
@@ -1,5 +1,9 @@
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
 import { options } from 'preact';
 import type { VNode } from 'preact';
+
 import { COMPONENT, DIFF, DIFFED, FORCE } from '../renderToOpcodes/constants.js';
 
 const sForcedVNode = Symbol('FORCE');
