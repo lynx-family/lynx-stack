5ea2712a7570ec2aeefe0ccc9a3ba98f9a5dcb12
Yradex <11014207+Yradex@users.noreply.github.com>
2025-06-20 16:53:30 +1000

    Update packages/react/runtime/src/lifecycle/event/delayLifecycleEvents.ts
    Co-authored-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
    Signed-off-by: Yradex <11014207+Yradex@users.noreply.github.com>

--- packages/react/runtime/src/lifecycle/event/delayLifecycleEvents.ts
+++ packages/react/runtime/src/lifecycle/event/delayLifecycleEvents.ts
@@ -1,7 +1,7 @@
 // Copyright 2025 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
-import { LifecycleConstant } from '../../lifecycleConstant.js';
+import type { LifecycleConstant } from '../../lifecycleConstant.js';
 
 const delayedLifecycleEvents: [type: LifecycleConstant, data: unknown][] = [];
 
