04a1848db49dbf4bc7144d52940cfc8aa6d40d29
Yradex <11014207+Yradex@users.noreply.github.com>
2025-06-20 17:19:07 +0800

    auto fix

--- packages/react/runtime/src/snapshot/platformInfo.ts
+++ packages/react/runtime/src/snapshot/platformInfo.ts
@@ -1,4 +1,7 @@
-import { __pendingListUpdates, ListUpdateInfoRecording } from '../list.js';
+// Copyright 2025 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+import { ListUpdateInfoRecording, __pendingListUpdates } from '../list.js';
 import { SnapshotInstance } from '../snapshot.js';
 
 const platformInfoVirtualAttributes: Set<string> = /* @__PURE__ */ new Set<string>(['reuse-identifier']);
