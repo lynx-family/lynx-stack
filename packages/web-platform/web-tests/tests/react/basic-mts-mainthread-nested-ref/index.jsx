2ac31640fe544c9dc84d66abfbb499358ef1668b
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-07-08 17:11:27 +0800

    fix(react/worklet-runtime): should allow nested MTS ref

---
+++ packages/web-platform/web-tests/tests/react/basic-mts-mainthread-nested-ref/index.jsx
@@ -0,0 +1,38 @@
+// Copyright 2023 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+import { root, useCallback, useMainThreadRef } from '@lynx-js/react';
+
+function Foo(props) {
+  const { mainThreadSetRef } = props;
+
+  function setRef(element) {
+    'main thread';
+    mainThreadSetRef(element);
+  }
+
+  return (
+    <view
+      main-thread:ref={setRef}
+      id='target'
+      style={{ backgroundColor: 'rgb(0, 128, 0)' }}
+    />
+  );
+}
+
+export function App() {
+  const ref = useMainThreadRef([]);
+
+  const setRef = useCallback(function setRef(element) {
+    'main thread';
+
+    ref.current[0] = element;
+  }, []);
+
+  return (
+    <view main-thread:ref={setRef}>
+      <Foo mainThreadSetRef={setRef} />
+    </view>
+  );
+}
+root.render(<App />);
