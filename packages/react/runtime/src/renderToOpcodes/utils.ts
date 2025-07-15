2a0db03324e7db7a3b36b8997b0d77ce7068fdaf
Yradex <11014207+Yradex@users.noreply.github.com>
2025-07-15 10:55:58 +0800

    make ai happy

--- packages/react/runtime/src/renderToOpcodes/utils.ts
+++ packages/react/runtime/src/renderToOpcodes/utils.ts
@@ -1,13 +1,10 @@
-/**
- * @template T
- */
-export class Deferred {
-  promise: Promise<any>;
-  resolve: any;
-  reject: any;
+export class Deferred<T> {
+  promise: Promise<T>;
+  resolve!: (value: T | PromiseLike<T>) => void;
+  reject!: (reason?: any) => void;
 
   constructor() {
-    this.promise = new Promise((resolve, reject) => {
+    this.promise = new Promise<T>((resolve, reject) => {
       this.resolve = resolve;
       this.reject = reject;
     });
