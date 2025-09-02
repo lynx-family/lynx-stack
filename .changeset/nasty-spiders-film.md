97d7fd6e1432ad1debffac54d085424d985ac627
Sherry-hue <37186915+Sherry-hue@users.noreply.github.com>
2025-09-02 17:22:03 +0800

    feat(web): WebEncodePlugin outputs add new field: appType, version

---
+++ .changeset/nasty-spiders-film.md
@@ -0,0 +1,6 @@
+---
+"@lynx-js/template-webpack-plugin": patch
+"@lynx-js/react-rsbuild-plugin": patch
+---
+
+feat: web output add `version` field to identify the build version `appType` field to identify lazy-bundle; The WrapperPlugin is also enabled on the Web.
--- packages/webpack/template-webpack-plugin/src/WebEncodePlugin.ts
+++ packages/webpack/template-webpack-plugin/src/WebEncodePlugin.ts
@@ -98,6 +98,10 @@ export class WebEncodePlugin {
               },
               customSections: encodeOptions.customSections,
               elementTemplate: encodeOptions['elementTemplate'],
+              appType: (encodeOptions['pageConfig'] as Record<string, unknown>)
+                  ?.['experimental_isLazyBundle']
+                ? 'lazy'
+                : 'card',
             })),
             debugInfo: '',
           };
