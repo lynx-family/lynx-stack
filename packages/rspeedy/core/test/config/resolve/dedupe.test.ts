d7f5dfb700dc580aeb2f2621676ff22d731875b4
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-09-08 20:20:16 +0800

    fix: Windows

--- packages/rspeedy/core/test/config/resolve/dedupe.test.ts
+++ packages/rspeedy/core/test/config/resolve/dedupe.test.ts
@@ -2,6 +2,8 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 
+import path from 'node:path'
+
 import { describe, expect, test } from 'vitest'
 
 import { createStubRspeedy } from '../../createStubRspeedy.js'
@@ -29,7 +31,7 @@ describe('Config - Resolve.dedupe', () => {
 
     expect(config.resolve?.alias).toHaveProperty(
       '@rsbuild/core',
-      expect.stringContaining('@rsbuild/core'),
+      expect.stringContaining(`@rsbuild${path.sep}core`),
     )
   })
 
