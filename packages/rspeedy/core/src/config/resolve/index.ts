2708fae8eba17749712220d97a831a645dc26aaa
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2025-09-11 17:29:33 +0800

    fix: test

--- packages/rspeedy/core/src/config/resolve/index.ts
+++ packages/rspeedy/core/src/config/resolve/index.ts
@@ -94,11 +94,10 @@ export interface Resolve {
   /**
    * Set the strategy for path alias resolution, to control the priority relationship
    * between the `paths` option in `tsconfig.json` and the `resolve.alias` option of Rsbuild.
-   * - `prefer-tsconfig`: The `paths` option in `tsconfig.json` will take precedence over the
+   * - `prefer-tsconfig` (default): The `paths` option in `tsconfig.json` will take precedence over the
    * `resolve.alias` option of Rsbuild.
    * - `prefer-alias`: The `resolve.alias` option of Rsbuild will take precedence over the
    * `paths` option in `tsconfig.json`.
-   * @default 'prefer-tsconfig'
    *
    * @example
    *
