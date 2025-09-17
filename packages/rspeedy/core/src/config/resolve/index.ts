2a36abae48cb78d337b47310f786ab1b024dcdd7
bab <mashirohira@gmail.com>
2025-09-17 17:21:22 +0900

    Apply suggestion from @colinaaa
    Co-authored-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
    Signed-off-by: bab <mashirohira@gmail.com>

--- packages/rspeedy/core/src/config/resolve/index.ts
+++ packages/rspeedy/core/src/config/resolve/index.ts
@@ -144,7 +144,7 @@ export interface Resolve {
   /**
    * Automatically resolve file extensions when importing modules. This means you can import files without explicitly writing their extensions.
    *
-   * default: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json', '.cjs']
+   * Default: `['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json', '.cjs']`
    *
    * For example, if importing './index', Rsbuild will try to resolve using the following order:
    *
