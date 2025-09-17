2a41770c1b9f8a8c9833c31c9aa5e7bca744afd1
bab <mashirohira@gmail.com>
2025-09-17 17:21:28 +0900

    Apply suggestion from @colinaaa
    Co-authored-by: Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
    Signed-off-by: bab <mashirohira@gmail.com>

--- packages/rspeedy/core/src/config/resolve/index.ts
+++ packages/rspeedy/core/src/config/resolve/index.ts
@@ -148,13 +148,19 @@ export interface Resolve {
    *
    * For example, if importing './index', Rsbuild will try to resolve using the following order:
    *
-   * ./index.ts
-   * ./index.tsx
-   * ./index.mjs
-   * ./index.js
-   * ./index.jsx
-   * ./index.json
-   * ./index.cjs
+   * - `./index.ts`
+   *
+   * - `./index.tsx`
+   *
+   * - `./index.mjs`
+   *
+   * - `./index.js`
+   *
+   * - `./index.jsx`
+   *
+   * - `./index.json`
+   *
+   * - `./index.cjs`
    *
    * @example
    * ```js
