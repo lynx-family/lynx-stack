76e46491f9025bd6184ce773a194b63d3e2d8fc1
babu-ch <mashirohira@gmail.com>
2025-09-19 20:59:52 +0900

    feat(rspeedy): Support server.compress

--- packages/rspeedy/core/src/config/rsbuild/index.ts
+++ packages/rspeedy/core/src/config/rsbuild/index.ts
@@ -88,6 +88,8 @@ export function toRsbuildConfig(
     server: {
       base: config.server?.base,
 
+      compress: config.server?.compress ?? true,
+
       headers: config.server?.headers,
 
       host: config.server?.host,
