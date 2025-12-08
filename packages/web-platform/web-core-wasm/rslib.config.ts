2f88a07772318729cb6da33683a95df20c1e7e00
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-12-09 02:48:17 +0800

    + rslib

---
+++ packages/web-platform/web-core-wasm/rslib.config.ts
@@ -0,0 +1,61 @@
+import { defineConfig } from '@rslib/core';
+// import './scripts/build.js';
+
+export default defineConfig({
+  lib: [
+    {
+      format: 'esm',
+      source: {
+        entry: {
+          index: './ts/encode/index.ts',
+        },
+        tsconfigPath: './tsconfig.build.json',
+      },
+      output: {
+        target: 'node',
+        distPath: {
+          root: './dist/encode',
+        },
+        minify: true,
+      },
+      tools: {
+        rspack: {
+          target: 'node20',
+        },
+      },
+    },
+    {
+      format: 'esm',
+      source: {
+        entry: {
+          index: './ts/client/index.ts',
+          background: './ts/client/background/index.ts',
+        },
+        tsconfigPath: './tsconfig.build.json',
+      },
+      output: {
+        target: 'web',
+        distPath: {
+          root: './dist/client',
+        },
+        minify: true,
+      },
+      autoExternal: false,
+      tools: {
+        rspack: {
+          target: [
+            'web',
+            'browserslist:Chrome > 92',
+            'browserslist:Safari > 16.4',
+          ],
+          output: {
+            publicPath: 'auto',
+          },
+          experiments: {
+            asyncWebAssembly: true,
+          },
+        },
+      },
+    },
+  ],
+});
---
+++ packages/web-platform/web-core-wasm/ts/client/index.ts
@@ -0,0 +1,9 @@
+import(
+  /* webpackChunkName: "web-core-main-chunk" */
+  './mainthread/LynxView.js'
+);
+import(
+  /* webpackChunkName: "web-core-wasm-file" */
+  '../../binary/client/client_bg.wasm'
+);
+import '../../index.css';
