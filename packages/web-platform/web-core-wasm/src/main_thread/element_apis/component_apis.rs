5d68168cc8ac4dd585811e5fc3018ce2efc7e872
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-12-24 12:04:57 +0800

    + unwrap fix by ai

--- packages/web-platform/web-core-wasm/src/main_thread/element_apis/component_apis.rs
+++ packages/web-platform/web-core-wasm/src/main_thread/element_apis/component_apis.rs
@@ -4,23 +4,27 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 impl MainThreadWasmContext {
   #[wasm_bindgen(js_name = "__GetComponentID")]
-  pub fn get_component_id(&self, unique_id: usize) -> Option<String> {
-    self
-      .get_element_data_by_unique_id(unique_id)
-      .unwrap()
-      .borrow()
-      .component_id
-      .clone()
+  pub fn get_component_id(&self, unique_id: usize) -> Result<Option<String>, JsError> {
+    Ok(
+      self
+        .get_element_data_by_unique_id(unique_id)
+        .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?
+        .borrow()
+        .component_id
+        .clone(),
+    )
   }
 
   #[wasm_bindgen(js_name = "__GetElementConfig")]
-  pub fn get_element_config(&self, unique_id: usize) -> Option<js_sys::Object> {
-    self
-      .get_element_data_by_unique_id(unique_id)
-      .unwrap()
-      .borrow()
-      .component_config
-      .clone()
+  pub fn get_element_config(&self, unique_id: usize) -> Result<Option<js_sys::Object>, JsError> {
+    Ok(
+      self
+        .get_element_data_by_unique_id(unique_id)
+        .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?
+        .borrow()
+        .component_config
+        .clone(),
+    )
   }
 
   #[wasm_bindgen(js_name = "__SetConfig")]
@@ -28,33 +32,41 @@ impl MainThreadWasmContext {
    * key: String
    * value: stringifyed js value
    */
-  pub fn set_config(&self, unique_id: usize, config: &js_sys::Object) {
+  pub fn set_config(&self, unique_id: usize, config: &js_sys::Object) -> Result<(), JsError> {
     self
       .get_element_data_by_unique_id(unique_id)
-      .unwrap()
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?
       .borrow_mut()
       .component_config = Some(config.clone());
+    Ok(())
   }
 
   #[wasm_bindgen(js_name = "__GetConfig")]
-  pub fn get_config(&self, unique_id: usize) -> js_sys::Object {
-    let binding = self.get_element_data_by_unique_id(unique_id).unwrap();
+  pub fn get_config(&self, unique_id: usize) -> Result<js_sys::Object, JsError> {
+    let binding = self
+      .get_element_data_by_unique_id(unique_id)
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?;
     let mut element_data = binding.borrow_mut();
     if let Some(config) = &element_data.component_config {
-      config.clone()
+      Ok(config.clone())
     } else {
       let js_obj = js_sys::Object::new();
       element_data.component_config = Some(js_obj.clone());
-      js_obj
+      Ok(js_obj)
     }
   }
 
   #[wasm_bindgen(js_name = "__UpdateComponentID")]
-  pub fn update_component_id(&self, unique_id: usize, component_id: Option<String>) {
+  pub fn update_component_id(
+    &self,
+    unique_id: usize,
+    component_id: Option<String>,
+  ) -> Result<(), JsError> {
     self
       .get_element_data_by_unique_id(unique_id)
-      .unwrap()
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?
       .borrow_mut()
       .component_id = component_id;
+    Ok(())
   }
 }
