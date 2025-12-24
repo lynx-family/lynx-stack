297db8fc90532f06201fa793b47baff8768ca0e1
pupiltong <12288479+PupilTong@users.noreply.github.com>
2025-12-24 12:14:17 +0800

    feat: Improve dataset API robustness by returning `Result<T, JsError>` for element lookup failures and invalid string conversions.

--- packages/web-platform/web-core-wasm/src/main_thread/element_apis/dataset_apis.rs
+++ packages/web-platform/web-core-wasm/src/main_thread/element_apis/dataset_apis.rs
@@ -9,8 +9,10 @@ impl MainThreadWasmContext {
     unique_id: usize,
     dom: &web_sys::HtmlElement,
     new_dataset: &js_sys::Object,
-  ) {
-    let element_rc = self.get_element_data_by_unique_id(unique_id).unwrap();
+  ) -> Result<(), JsError> {
+    let element_rc = self
+      .get_element_data_by_unique_id(unique_id)
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?;
     let mut element_data = element_rc.borrow_mut();
     let dataset = element_data.dataset.get_or_insert_with(js_sys::Object::new);
     // compare old dataset and new dataset and update dom attributes
@@ -20,8 +22,9 @@ impl MainThreadWasmContext {
     for i in 0..old_keys.length() {
       let key = old_keys.get(i);
       if !js_sys::Reflect::has(new_dataset, &key).unwrap_or(false) {
-        let key_str = key.as_string().unwrap();
-        let _ = dom.remove_attribute(&format!("data-{key_str}"));
+        if let Some(key_str) = key.as_string() {
+          let _ = dom.remove_attribute(&format!("data-{key_str}"));
+        }
       }
     }
     // set/ update new keys
@@ -32,16 +35,18 @@ impl MainThreadWasmContext {
       let old_value =
         js_sys::Reflect::get(dataset, &key).unwrap_or(wasm_bindgen::JsValue::UNDEFINED);
       if old_value != new_value {
-        let key_str = key.as_string().unwrap();
-        if new_value.is_undefined() || new_value.is_null() {
-          let _ = dom.remove_attribute(&format!("data-{key_str}"));
-        } else {
-          let value_str = new_value.as_string().unwrap_or_default();
-          let _ = dom.set_attribute(&format!("data-{key_str}"), &value_str);
+        if let Some(key_str) = key.as_string() {
+          if new_value.is_undefined() || new_value.is_null() {
+            let _ = dom.remove_attribute(&format!("data-{key_str}"));
+          } else {
+            let value_str = new_value.as_string().unwrap_or_default();
+            let _ = dom.set_attribute(&format!("data-{key_str}"), &value_str);
+          }
         }
       }
     }
     element_data.dataset = Some(new_dataset.clone());
+    Ok(())
   }
 
   #[wasm_bindgen(js_name = "__AddDataset")]
@@ -50,9 +55,11 @@ impl MainThreadWasmContext {
     unique_id: usize,
     key: &wasm_bindgen::JsValue,
     value: &wasm_bindgen::JsValue,
-  ) {
+  ) -> Result<(), JsError> {
     // get the dataset object, create one if not exists
-    let element_rc = self.get_element_data_by_unique_id(unique_id).unwrap();
+    let element_rc = self
+      .get_element_data_by_unique_id(unique_id)
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?;
     let mut element_data = element_rc.borrow_mut();
     let dataset_obj = if let Some(dataset) = element_data.dataset.take() {
       dataset
@@ -61,31 +68,40 @@ impl MainThreadWasmContext {
     };
     let _ = js_sys::Reflect::set(&dataset_obj, key, value);
     element_data.dataset = Some(dataset_obj);
+    Ok(())
   }
 
   #[wasm_bindgen(js_name = "__GetDataset")]
-  pub fn get_dataset(&self, unique_id: usize) -> js_sys::Object {
-    let element_rc = self.get_element_data_by_unique_id(unique_id).unwrap();
+  pub fn get_dataset(&self, unique_id: usize) -> Result<js_sys::Object, JsError> {
+    let element_rc = self
+      .get_element_data_by_unique_id(unique_id)
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?;
     let element_data = element_rc.borrow();
     if let Some(dataset) = &element_data.dataset {
-      dataset.clone()
+      Ok(dataset.clone())
     } else {
-      js_sys::Object::new()
+      Ok(js_sys::Object::new())
     }
   }
 
   #[wasm_bindgen(js_name = "__GetDataByKey")]
-  pub fn get_data_by_key(&self, unique_id: usize, key: &str) -> wasm_bindgen::JsValue {
-    let element_rc = self.get_element_data_by_unique_id(unique_id).unwrap();
+  pub fn get_data_by_key(
+    &self,
+    unique_id: usize,
+    key: &str,
+  ) -> Result<wasm_bindgen::JsValue, JsError> {
+    let element_rc = self
+      .get_element_data_by_unique_id(unique_id)
+      .ok_or_else(|| JsError::new(&format!("Element {unique_id} not found")))?;
     let element_data = element_rc.borrow();
     if let Some(dataset) = &element_data.dataset {
       if let Ok(value) = js_sys::Reflect::get(dataset, &wasm_bindgen::JsValue::from_str(key)) {
-        value
+        Ok(value)
       } else {
-        wasm_bindgen::JsValue::UNDEFINED
+        Ok(wasm_bindgen::JsValue::UNDEFINED)
       }
     } else {
-      wasm_bindgen::JsValue::UNDEFINED
+      Ok(wasm_bindgen::JsValue::UNDEFINED)
     }
   }
 }
