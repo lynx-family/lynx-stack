f864ddce83ef891cbf22237b147379fcac5e7d70
pupiltong <12288479+PupilTong@users.noreply.github.com>
2026-03-23 12:15:55 +0800

    + tests

--- packages/testing-library/kitten-lynx/tests/lynx.spec.ts
+++ packages/testing-library/kitten-lynx/tests/lynx.spec.ts
@@ -10,6 +10,7 @@ const cwd = path.dirname(__dirname);
 import { AdbServerClient, type Adb } from '@yume-chan/adb';
 import { AdbServerNodeTcpConnector } from '@yume-chan/adb-server-node-tcp';
 import { execa } from 'execa';
+import type { KittenLynxView } from '../src/KittenLynxView.js';
 execa({
   env: {
     ...process.env,
@@ -25,6 +26,7 @@ execa({
 describe('kitten-lynx testing framework', () => {
   let lynx: Lynx;
   let adb: Adb;
+  let page: KittenLynxView;
 
   beforeAll(async () => {
     // Use ADB port forwarding
@@ -42,28 +44,56 @@ describe('kitten-lynx testing framework', () => {
     }
 
     lynx = await Lynx.connect();
-  });
+
+    page = await lynx.newPage();
+    await page.goto('http://127.0.0.1:3001/react-example.lynx.bundle', {
+      timeout: 15000,
+    });
+  }, 90000);
 
   afterAll(async () => {
     await lynx?.close();
   });
 
   it('can navigate to a page and read the DOM', async () => {
-    const page = await lynx.newPage();
-
-    await page.goto('http://127.0.0.1:3001/react-example.lynx.bundle', {
-      timeout: 15000,
-    });
-
     const content = await page.content();
     expect(content).toContain('have fun');
+  });
 
-    const rootElement = await page.locator('view');
-    expect(rootElement).toBeDefined();
+  it('can get attributes from an element', async () => {
+    const titleElement = await page.locator('.Title');
+    expect(titleElement).toBeDefined();
 
-    if (rootElement) {
-      const styles = await rootElement.computedStyleMap();
-      expect(styles.size).toBeGreaterThan(0);
+    if (titleElement) {
+      const classAttr = await titleElement.getAttribute('class');
+      expect(classAttr).toBe('Title');
+
+      const textAttr = await titleElement.getAttribute('text');
+      expect(textAttr).toBe('React');
     }
-  }, 90000); // Increase timeout to 90s as connecting/launching emulator app can be slow
+  });
+
+  it('can fetch computed style map of an element', async () => {
+    const titleElement = await page.locator('.Title');
+    expect(titleElement).toBeDefined();
+
+    if (titleElement) {
+      const styles = await titleElement.computedStyleMap();
+      expect(styles.size).toBeGreaterThan(0);
+      expect(styles.has('display')).toBe(true);
+    }
+  });
+
+  it('can click', async () => {
+    const logoParent = await page.locator('.Logo');
+    expect(await page.locator('.Logo--lynx')).toBeDefined();
+    await logoParent?.tap();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    expect(await page.locator('.Logo--react')).toBeDefined();
+    expect(await page.locator('.Logo--lynx')).toBeUndefined();
+    await logoParent?.tap();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    expect(await page.locator('.Logo--react')).toBeUndefined();
+    expect(await page.locator('.Logo--lynx')).toBeDefined();
+  });
 });
