c140079276ba6456a35cd77fa0c54e95b9c92ed6
yradex <11014207+Yradex@users.noreply.github.com>
2026-03-30 16:59:33 +0800

    refactor(worklet-runtime): forward top-level bootstrap

--- packages/react/worklet-runtime/src/index.ts
+++ packages/react/worklet-runtime/src/index.ts
@@ -1,12 +1,4 @@
 // Copyright 2024 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
-import { initApiEnv } from '../../runtime/src/worklet-runtime/api/lynxApi.js';
-import { initEventListeners } from '../../runtime/src/worklet-runtime/listeners.js';
-import { initWorklet } from '../../runtime/src/worklet-runtime/workletRuntime.js';
-
-if (globalThis.lynxWorkletImpl === undefined) {
-  initWorklet();
-  initApiEnv();
-  initEventListeners();
-}
+import '../../runtime/src/worklet-runtime/index.js';
