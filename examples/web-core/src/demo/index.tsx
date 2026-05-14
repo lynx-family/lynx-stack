53b86be49d423bed425e21bbdba5a5544963da35
Dugyu <23237626+Dugyu@users.noreply.github.com>
2026-05-14 19:18:55 +0800

    test: commit 6e4cae0, scroll drift fix, transform drift fix (excl first render)

--- examples/web-core/src/demo/index.tsx
+++ examples/web-core/src/demo/index.tsx
@@ -1,21 +1,18 @@
-// import { Stage, StageContainer } from '@dugyu/luna-stage';
-// import { LunaLynxStage } from '@dugyu/luna-stage/lynx';
-import { VanillaLynxView } from './vanilla.js';
+import { Stage, StageContainer } from '@dugyu/luna-stage';
+import { LunaLynxStage } from '@dugyu/luna-stage/lynx';
+// import { VanillaLynxView } from './vanilla.js';
 import './index.css';
 
 const demos = [
   'PopoverBasic',
   'PopoverExtraAnchor',
   'PopoverOffsetAdjustment',
-  'force_field',
 ];
 export default function Preview() {
   return (
     <div className='demo-stages'>
-      {
-        /*       {demos.map(demo => (
+      {demos.map(demo => (
         <div key={demo} className='demo-stage'>
-          <p className='stage-title'>{demo}</p>
           <StageContainer className='stage-container'>
             <Stage>
               <LunaLynxStage
@@ -26,25 +23,24 @@ export default function Preview() {
             </Stage>
           </StageContainer>
         </div>
-      ))} */
+      ))}
+
+      {
+        /*       <StageContainer className='stage-container'>
+        <Stage alignX='left' alignY='top'>
+          <LunaLynxStage
+            entry={'PopoverBasic'}
+            bundleRoot='/'
+            lunaTheme='lunaris-dark'
+          />
+        </Stage>
+      </StageContainer> */
       }
       {
-        /*  {
-        <StageContainer className='stage-container'>
-          <Stage alignX='left' alignY='top'>
-            <LunaLynxStage
-              entry={'PopoverBasic'}
-              bundleRoot='/'
-              lunaTheme='lunaris-dark'
-            />
-          </Stage>
-        </StageContainer>
-      } */
-      }
-
-      {demos.map(demo => {
+        /*       {demos.map(demo => {
         return <VanillaLynxView key={demo} entry={demo} bundleRoot='/' />;
-      })}
+      })} */
+      }
     </div>
   );
 }
