20e27e9ffd390964ceac03e9630525042ccb6f45
Haoyang Wang <12288479+PupilTong@users.noreply.github.com>
2026-06-18 14:05:04 +0800

    Clean up React fixture dependencies

--- packages/genui/ui-judge/tests/fixtures/react/src/index.tsx
+++ packages/genui/ui-judge/tests/fixtures/react/src/index.tsx
@@ -1,11 +1,6 @@
 // Copyright 2026 The Lynx Authors. All rights reserved.
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
-// Rspeedy aliases this fixture-only devtools package during bundling.
-// eslint-disable-next-line import/no-unresolved
-import '@lynx-js/preact-devtools';
-import '@lynx-js/react/debug';
-
 import { root } from '@lynx-js/react';
 
 import { App } from './App.jsx';
