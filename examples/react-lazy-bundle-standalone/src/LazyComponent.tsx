813307f82a94208199c0454d88a05e376f51b7ba
Yiming Li <yimingli.cs@gmail.com>
2026-08-12 11:44:20 +0800

    chore(examples): demo a merged snapshot and worklet inside the lazy bundles

--- examples/react-lazy-bundle-standalone/src/LazyComponent.tsx
+++ examples/react-lazy-bundle-standalone/src/LazyComponent.tsx
@@ -1,9 +1,11 @@
 import './LazyComponent.css';
+import { BackgroundOnlyCard } from './BackgroundOnlyCard.jsx';
 
 export default function LazyComponent() {
   return (
     <view>
       <text className='LazyComponent'>LazyComponent</text>
+      {__MAIN_THREAD__ ? <text>skeleton</text> : <BackgroundOnlyCard />}
     </view>
   );
 }
