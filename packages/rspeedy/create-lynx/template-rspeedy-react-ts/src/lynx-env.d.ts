f692707211faa981a434d50c65f38f7c6c7b0ee1
Yiming Li <yimingli.cs@gmail.com>
2026-09-10 20:12:46 +0800

    feat(create-lynx): declare GlobalProps in every TypeScript app template
    The declaration only augments `GlobalProps`, so it is not tied to a build
    tool. Rename it to `lynx-env.d.ts` and give the Rsbuild template one too.

---
+++ packages/rspeedy/create-lynx/template-rspeedy-react-ts/src/lynx-env.d.ts
@@ -0,0 +1,12 @@
+declare module '@lynx-js/types' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface GlobalProps {
+    /**
+     * Define your global properties in this interface.
+     * These types will be accessible through `lynx.__globalProps`.
+     */
+  }
+}
+
+// This export makes the file a module
+export {}
