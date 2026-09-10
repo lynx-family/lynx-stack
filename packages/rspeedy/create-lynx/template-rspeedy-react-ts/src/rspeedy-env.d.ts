a60d5a52a497c997791ce4ef18d7493b372ee57a
Yiming Li <yimingli.cs@gmail.com>
2026-09-10 14:45:13 +0800

    chore(create-lynx): load the build tool types through tsconfig instead of an env.d.ts

--- packages/rspeedy/create-lynx/template-rspeedy-react-ts/src/rspeedy-env.d.ts
+++ packages/rspeedy/create-lynx/template-rspeedy-react-ts/src/rspeedy-env.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="@lynx-js/rspeedy/client" />
-
 declare module '@lynx-js/types' {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface GlobalProps {
